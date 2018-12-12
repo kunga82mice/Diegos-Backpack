926a15c97f470d0940c95a58cb0c9f9342dcf71d
kunga82mice <44040639+kunga82mice@users.noreply.github.com>
2018-12-12 17:48:40 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$(".diegosbag").click(function()  {
+$(".supplies1").slideToggle();  
+});
+
+$(".bagpack").click(function()  {
+$(".supplies2").Toggle();
+});
+
+$(".bookbag").click(function()  {
+$(".supplies3").fadeToggle();
+});
